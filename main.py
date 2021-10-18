40f4e81c678bd52b5f5cdadba31fc9fe9e6397cf
Nicholas Darmawan <nicholasnick230@gmail.com>
2021-10-18 23:19:50 +0700

    coba4

--- main.py
+++ main.py
@@ -136,8 +136,6 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
-########################################################################################
-
 with open("menu.json", "r") as read_file: 
     data = json.load(read_file)
 
